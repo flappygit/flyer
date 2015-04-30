ea2dd7e5dc0c4c0c6f31d29df5dc1947264eb576
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-04-30 11:02:02 +0530

    Wrote first Kivy program. Works well. Pygame was a pain to install. Can't install from pip.

---
+++ modules/kivy_trial.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+#! -*- coding: utf-8 -*-
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+	def build(self):
+		return Label(text='Hello world')
+
+if __name__=="__main__":
+	MyApp().run()
