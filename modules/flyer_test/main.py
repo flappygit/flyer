518b0b05c5e0d934ebc1f3f8272695bf828f6356
Silver-Devil <abinavkrish@gmail.com>
2015-05-11 13:38:01 +0530

    Added the tab navigation to the app. Image is still not properly aligned to the button.

--- modules/flyer_test/main.py
+++ modules/flyer_test/main.py
@@ -11,12 +11,14 @@ from kivy.uix.widget import Widget
 
 class FlyerHeader(BoxLayout):
 	pass
+class FlyerTabNavigation(GridLayout):
+	pass
 
 class FlyerIntro(BoxLayout):
 	username = 'Pavan Kumar'
 	status = ' In Esteem Asrani'
 	last_updated = '30-04-2015 15:45'
-	daily_quote = 'Quote of The Day: \n\"The Supreme art of war is\n to subdue the enemy without\n \
+	daily_quote = '[b]Quote of The Day:[/b]  \"The Supreme art of war is to subdue the enemy without \
 fighting.\"\n [b]~ Sun Tzu[/b]'
 
 class FlyerRoot(BoxLayout):
