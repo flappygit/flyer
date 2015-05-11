63437d05e43bc54a1bdf901ec30c696452f0a0c4
Silver-Devil <abinavkrish@gmail.com>
2015-05-11 11:20:56 +0530

    Added the Header part of the App. I decided to Redo the entire app again with cleaner code.

--- modules/flyer_test/main.py
+++ modules/flyer_test/main.py
@@ -8,24 +8,18 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
-class AddHeader(BoxLayout):
+
+class FlyerHeader(BoxLayout):
 	pass
 
-class AddIntroduction(BoxLayout):
-	user = 'Pavan Kumar'
-	location = 'Esteem Asrani'
-	last_updated = '30-04-15 15:45'
-	daily_quote = '\"The supreme art of war is \n \
-to subdue the enemy without \nfighting.\" [b]~Sun Tzu[/b]'
-
-class TabNavigation(GridLayout):
-	pass
 
 class FlyerRoot(BoxLayout):
 	pass
 
+
 class FlyerApp(App):
 	pass
+
 
 if __name__ == '__main__':
 	FlyerApp().run()
