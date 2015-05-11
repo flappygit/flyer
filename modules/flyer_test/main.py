f2bcc0b086b711f8be2d43814f6b07cdf2088d68
Silver-Devil <abinavkrish@gmail.com>
2015-05-11 12:16:47 +0530

    Added the status section of the app.

--- modules/flyer_test/main.py
+++ modules/flyer_test/main.py
@@ -12,6 +12,12 @@ from kivy.uix.widget import Widget
 class FlyerHeader(BoxLayout):
 	pass
 
+class FlyerIntro(BoxLayout):
+	username = 'Pavan Kumar'
+	status = ' In Esteem Asrani'
+	last_updated = '30-04-2015 15:45'
+	daily_quote = 'Quote of The Day: \n\"The Supreme art of war is\n to subdue the enemy without\n \
+fighting.\"\n [b]~ Sun Tzu[/b]'
 
 class FlyerRoot(BoxLayout):
 	pass
