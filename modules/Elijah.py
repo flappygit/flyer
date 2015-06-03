9d2e9cea539925c3c7fd3314561fc88a8396604a
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-06-03 22:12:49 +0530

    Added the getLatestReports methods to Elijah.

--- modules/Elijah.py
+++ modules/Elijah.py
@@ -17,7 +17,20 @@ def addReport(report_type, report_text, reportee_id):
 	conn.commit()
 	conn.close()
 
-def fetchLatestReports(query_date):
+def fetchLatestReports():
+	conn = getFlyerConnector()
+	cursor = conn.cursor()
+	sqlcmd = """(SELECT * from `hr_reports` WHERE 
+		`time_stamp` = (SELECT MAX(`time_stamp`) from `hr_reports` WHERE `report_type`="Performance")) UNION
+		(SELECT * from `hr_reports` WHERE 
+		`time_stamp` = (SELECT MAX(`time_stamp`) from `hr_reports` WHERE `report_type`="OrgDev")) UNION
+		(SELECT * from `hr_reports` WHERE 
+		`time_stamp` = (SELECT MAX(`time_stamp`) from `hr_reports` WHERE `report_type`="Rewards"));"""
+	cursor.execute(sqlcmd)
+	data = cursor.fetchall()
+	conn.commit()
+	conn.close()
+	print data
 	pass
 
 def modifyReport(report_job_card, report_text):
