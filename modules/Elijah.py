eff48c9f04d31e5d26840d4cb1ccda24a7572b1f
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2015-06-04 23:29:45 +0530

    Modified Elijah to add login and getUserRecords methods.

--- modules/Elijah.py
+++ modules/Elijah.py
@@ -39,6 +39,7 @@ def fetchLatestReports():
 	return data
 
 def modifyReport(report_type, reportee_id, query_date, new_report_text):
+	"""Change the report text for a given date, report type and reportee_id."""
 	conn = getFlyerConnector()
 	cursor = conn.cursor()
 	start_time_stamp = datetime.datetime(query_date.year, query_date.month, query_date.day, 0, 0, 0)
@@ -50,3 +51,31 @@ def modifyReport(report_type, reportee_id, query_date, new_report_text):
 	data = cursor.fetchall()
 	conn.commit()
 	conn.close()
+
+def login(user_id, password):
+	"""Check if a password matches a user_id. 
+	Return True if it does, False either 
+	if the password is incorrect or if there's no such user."""
+	conn = getFlyerConnector()
+	cursor = conn.cursor()
+	sqlcmd = """SELECT `password` from `user_records` WHERE `user_id`="%s";"""%user_id
+	cursor.execute(sqlcmd)
+	data = cursor.fetchall()
+	conn.close()
+	if len(data) == 0:
+		return False
+	else:
+		return (data["password"] == password)
+
+def getUserDetails(user_id):
+	"""For a user_id, this gives the user_name and email. Returns false if such a user doesn't exist."""
+	conn = getFlyerConnector()
+	cursor = conn.cursor()
+	sqlcmd = """SELECT `user_name`, `user_email_id` FROM `user_records` WHERE `user_id`="%s";""" %user_id
+	cursor.execute(sqlcmd)
+	data = cursor.fetchall()
+	conn.close()
+	if len(data) == 0:
+		return False
+	else:
+		return data[0] #data is a tuple of dictionaries with len=1 in this case
