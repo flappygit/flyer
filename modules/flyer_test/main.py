9f048ed0675eda2030b6f8d1df2c1c3005e84001
Silver-Devil <abinavkrish@gmail.com>
2015-06-04 22:20:54 +0530

    Redid the UI and finished the user login page.

--- modules/flyer_test/main.py
+++ modules/flyer_test/main.py
@@ -1,48 +1,62 @@
-#/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+Documentation Goes Here.
+
+Author: Bukkaraya Abinav
+Last Edited: June 2015
+
+"""
+
 from kivy.app import App
-from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.factory import Factory
-from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout 
 from kivy.utils import get_color_from_hex
-
-
-class FlyerHeader(BoxLayout):
-	pass
-
-
-class FlyerTabs(BoxLayout):
-	tab_no = 1
-	if tab_no == 2 or tab_no == 3:
-		tab_no = 2(tab_no) + 1
-		
-	#organisation_tab = Factory.FlyerTab()
-	#organisation_tab.tab_no = 1
-	#organisation_tab.tab_name = '[b]Organisation Tab:[/b]'
-	#self.add_widget(organisation_tab)
-
-class FlyerIntro(BoxLayout):
-	username = 'Pavan Kumar'
-	status = ' In Esteem Asrani'
-	last_updated = '30-04-2015 15:45'
-	daily_quote = '[b]Quote of The Day:[/b]  \"The Supreme art of war is to subdue the enemy without \
-fighting.\"\n [b]~ Sun Tzu[/b]'
-
-
-class FlyerRoot(BoxLayout):
-	pass
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 
 
 class FlyerApp(App):
-	pass
+	userIsValid = False
+
+
+	def login(self, userId, password):
+		return True
+
+	def getUserDetails(self):
+		self.userId = self.root.ids.userId.text
+		self.password = self.root.ids.password.text
+
+		print self.userId
+		print self.password
+
+		self.userIsValid = self.login(self.userId, self.password)
+
+		print self.userIsValid
+
+		if self.userIsValid:
+			changeLayout()
+		else:
+			showError()
+
+
+	def changeLayout(self):
+		pass
+
+	def showError(self):
+		pass
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-	Window.clearcolor = get_color_from_hex('#272A3E')
+	Window.clearcolor = get_color_from_hex('#0088D6')
+	LabelBase.register(name='Riona',
+                       fn_regular='RionaSans-Regular.ttf',
+                       fn_bold='RionaSans-Bold.ttf')
 	FlyerApp().run()
